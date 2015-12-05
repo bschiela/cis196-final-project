b59abfad904105a6bca2e3cff1e059f9f7bedcca
William Schiela <bschiela@gmail.com>
2015-12-05 16:36:43 -0500

    added user model with bcrypt and user migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ActiveRecord::Base
+  # validations
+  validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 15 }
+  validates :name, presence: true
+  validates :password_hash, presence: true
+  validates :email, presence: true
+
+  # users.password_hash in the database is a :string
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash) unless password_hash.nil?
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
---
+++ db/migrate/20151205213402_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :name
+      t.string :password_hash
+      t.string :email
+      t.string :bio
+      t.string :genres
+      t.string :image_url
+    end
+  end
+end
