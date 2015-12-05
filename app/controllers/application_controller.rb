1de562b46291fb98bb9296cf4da178689b94f561
William Schiela <bschiela@gmail.com>
2015-12-05 18:14:08 -0500

    added session helper methods to application_controller.rb and modified navigation menu to show current user if logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method def logged_in?
+    session[:user_id]
+  end
+
+  helper_method def current_user
+    @user ||= User.find(session[:user_id]) if logged_in?
+  end
 end
