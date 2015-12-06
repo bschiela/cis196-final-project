fc9b279b07512952b9a507771f2f67779ac2a6be
William Schiela <bschiela@gmail.com>
2015-12-05 19:10:12 -0500

    configured session routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   resources :users
 
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/logout' => 'session#destroy'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
