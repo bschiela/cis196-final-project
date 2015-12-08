68bb789747bbc50c5ac0f2d6dbe123fc370334a0
William Schiela <bschiela@gmail.com>
2015-12-08 18:22:55 -0500

    implemented playlist model with association to user

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,3 +1,14 @@
 class Playlist < ActiveRecord::Base
+  # associations
   belongs_to :user
+
+  # associated image
+  has_attached_file :image,
+    styles: { medium: "300x300>", thumb: "100x100>" },
+    default_url: "/images/playlist_image.png"
+  validates_attachment_content_type :image, content_type: /\Aimage/
+  validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/]
+
+  # validations
+  validates :name, presence: true
 end
