df0acf7b0a4e2e70ffbd82a677922e94ce08372c
William Schiela <bschiela@gmail.com>
2015-12-09 07:13:46 -0500

    added homepage featured content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
+  def index
+    @users = User.limit(8)
+    @sounds = Sound.limit(8)
+    @playlists = Playlist.limit(8)
+    render :index
+  end
 end
