aafa5b03d3215af864c8074a7d25c33505902fb5
William Schiela <bschiela@gmail.com>
2015-12-08 20:29:58 -0500

    added migration to attaach image to event, ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208231818) do
+ActiveRecord::Schema.define(version: 20151209012906) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "location"
+    t.datetime "start_datetime"
+    t.datetime "end_datetime"
+    t.integer  "user_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  add_index "events", ["user_id"], name: "index_events_on_user_id"
 
   create_table "playlists", force: :cascade do |t|
     t.string   "name"
